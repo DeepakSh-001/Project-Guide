b1a7b0f084d02907c7659e5bb3e016abcae7cc99
DeepakSh-001 <sharma.deepak011199@gmail.com>
2025-12-31 20:32:31 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -86,5 +86,5 @@ function setupDashboardButton(role) {
 logoutLink.addEventListener("click", async () => {
   await signOut(auth);
   localStorage.clear();
-  window.location.href = "login.html";
+  window.location.href = "index.html";
 });
